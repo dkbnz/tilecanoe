19adad7397ae3a0794e5b165d6aeae879cdc3f64
Doug Barrett <mail+git@dkb.nz>
2020-04-28 00:42:26 -0500

    Shaping docker-compose.yml

--- tippecanoe/entrypoint.sh
+++ tippecanoe/entrypoint.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-tippecanoe -zg -f -o /data/locations.mbtiles --cluster-distance=10 /data/locations.csv
+currentChecksum=$(sha1sum locations.csv)
+previousChecksum=$(sed "1q;d" metadata)
+
+if [ "$currentChecksum" != "$previousChecksum" ]; then
+  sleep 10
+  # Recalculate tiles
+  tippecanoe -zg -f -o /data/locations.mbtiles --cluster-distance=10 /data/locations.csv
+  # Write current hash to metadata file
+  sed -i "1s/.*/$currentChecksum/" metadata
+else
+  echo "No new locations."
+fi
